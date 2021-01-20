35c49b35b299543f63de8fd238c8cc920d516c9c
Tatyana Vinokurova <tvinokurova.exceedteam@gmail.com>
2021-01-20 12:18:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Использование методов работы с массивами
+
+let Slice = [2,33,33,45,4,5,6,7,8,33];
+console.log(Slice.slice(-2));
+////
+
+let Splice = [2,33,33,45,4,5,6,7,8,33];
+console.log(Splice.splice(-1,1));
+////
+
+let mp = [2,33,33,45,4,5,6,7,8,33].map(console.log(mp.length));
+
